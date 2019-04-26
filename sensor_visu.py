6ab3a8e6465fc450d1981a3dac19fd50a85ceb4b
Anton Höß <anton.hoess@fkie.fraunhofer.de>
2019-04-26 18:47:29 +0200

    docs: Add some to-do's.

--- sensor_visu.py
+++ sensor_visu.py
@@ -21,20 +21,24 @@ class SensorVisu:
                                      self.sensor.r[1] - self._EDGE_LENGHT / 2.,
                                      self.sensor.r[0] + self._EDGE_LENGHT / 2.,
                                      self.sensor.r[1] + self._EDGE_LENGHT / 2.,
-                                     fill=self.color)
+                                     fill=self.color, activefill="red")
         self.canvas.create_text(self.sensor.r[0], self.sensor.r[1], text=self.sensor.name, anchor=tk.CENTER)
 
         self.sensor.measurements = self.sensor.measurements[-self._meas_buf_max:]
 
         # The sensor's measurements
-        x_style = 2
+        x_style = 1
         for meas in self.sensor.measurements:
             if x_style == 0:  # Point
-                self.canvas.create_oval(meas.r[0], meas.r[1], meas.r[0], meas.r[1], width=3)
+                self.canvas.create_oval(meas.r[0], meas.r[1], meas.r[0], meas.r[1], width=5, outline="black")
+                self.canvas.create_oval(meas.r[0], meas.r[1], meas.r[0], meas.r[1], width=3, outline=self.color)
             elif x_style == 1:  # \ + /
                 x = 100
-                self.canvas.create_line(meas.r[0] - x, meas.r[1] - x, meas.r[0] + x, meas.r[1] + x, width=2)
-                self.canvas.create_line(meas.r[0] - x, meas.r[1] + x, meas.r[0] + x, meas.r[1] - x, width=2)
+                self.canvas.create_line(meas.r[0] - x, meas.r[1] - x, meas.r[0] + x, meas.r[1] + x, width=3, fill="black")
+                self.canvas.create_line(meas.r[0] - x, meas.r[1] + x, meas.r[0] + x, meas.r[1] - x, width=3, fill="black")
+
+                self.canvas.create_line(meas.r[0] - x, meas.r[1] - x, meas.r[0] + x, meas.r[1] + x, width=1, fill=self.color)
+                self.canvas.create_line(meas.r[0] - x, meas.r[1] + x, meas.r[0] + x, meas.r[1] - x, width=1, fill=self.color)
             else:  # ❌
                 self.canvas.create_text(meas.r[0], meas.r[1], text="❌", anchor=tk.CENTER, font=(None, 12), fill="black")
                 self.canvas.create_text(meas.r[0], meas.r[1], text="❌", anchor=tk.CENTER, font=(None, 8), fill=self.color)

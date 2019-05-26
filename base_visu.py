feat: Add visualization for radar measuremnt covariance matrix and adapted the covariance matrix values feat: Make correct rotation for non-rotating sensors feat: Add properties for the measurements that allow to use names for the measurement components, instead of only use is as a numpy-vector

base_visu.py
@@ -62,14 +62,21 @@ class BaseVisu:
                                fill=fill, capstyle=capstyle, arrow=arrow, **kwargs)
         # end for
 
-    def draw_cov_mat_ell(self, vehicle, cov_mat, cov_ell_cnt, fill, orient=False):
+    def draw_cov_mat_ell(self, sensor, vehicle, cov_mat, cov_ell_cnt, fill, orient=False):
         # The covariance ellipses
         if vehicle.active and cov_ell_cnt > 0:
-            theta = ISensor.calc_rotation_angle(vehicle) if orient else 0
+            if not sensor is None and orient:
+                theta = sensor.calc_rotation_angle(vehicle)
+            else:
+                theta = 0
+
+            rad = np.linalg.norm(vehicle.r - sensor.pos)
 
             # Calculate values for drawing the cov ellipse
-            cov_r_theta, cov_r_r1, cov_r_r2 = ISensor.calc_cov_ell_params_2d(np.asarray(cov_mat))
+            cov_r_theta, cov_r_r1, cov_r_r2 = ISensor.calc_cov_ell_params_2d(cov_mat)
             cov_r_theta += theta
+            cov_r_r1 *= rad
+            cov_r_r2 *= rad * math.pi
 
             for sigma in range(1, cov_ell_cnt + 1):
                 self.canvas.create_oval_rotated(vehicle.r[0], vehicle.r[1], cov_r_r1 * sigma, cov_r_r2 * sigma,
@@ -78,6 +85,7 @@ class BaseVisu:
                                                 cov_r_theta, n_segments=20, fill="", width=1, outline=fill)
             # end for
         # end if
+
 
 class TraceVisu(abc.ABC):
     def __init__(self, trace_length_max=10):

sensor.py
@@ -15,12 +15,28 @@ class PlaneMeasurement(Measurement):
         super().__init__(vehicle)
         self.val = pos
 
+    @property
+    def x(self):
+        return self.val[0]
+
+    @property
+    def y(self):
+        return self.val[1]
+
 
 # A measurement in polar coordinates
 class RadarMeasurement(Measurement):
     def __init__(self, vehicle, pos):
         super().__init__(vehicle)
         self.val = pos
+
+    @property
+    def rad(self):
+        return self.val[0]
+
+    @property
+    def phi(self):
+        return self.val[1]
 
 
 class ISensorMeasure(abc.ABC):
@@ -109,8 +125,6 @@ class Plane(ISensor, ISensorMeasure):
         meas = np.random.multivariate_normal(vehicle.r, self.cov_mat, 1)[0]
         self.append_measurement(PlaneMeasurement(vehicle, meas))
 
-        return meas
-
 
 class Radar(ISensor, ISensorMeasure):
     def __init__(self, name, active, pos, meas_interval, cov_mat):
@@ -118,9 +132,5 @@ class Radar(ISensor, ISensorMeasure):
         ISensorMeasure.__init__(self, meas_interval, cov_mat)
 
     def measure(self, vehicle, **kwargs):
-         pass
-#         # Measure azimuth and range
-#         theta = self.calc_rotation_angle(vehicle)
-#         R = self.calc_rotation_matrix_2d(theta)
-#         # meas_res = np.dot(R, np.random.multivariate_normal(vehicle.r, cov_r, 1)[0])
-#         # ...
+        meas = np.random.multivariate_normal(vehicle.r, self.cov_mat, 1)[0]
+        self.append_measurement(RadarMeasurement(vehicle, meas))
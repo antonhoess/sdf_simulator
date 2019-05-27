9b609ba4701c6c5ccf36a46c8b4dbdcb57124fe1
Anton Höß <anton.hoess@fkie.fraunhofer.de>
2019-05-27 13:37:01 +0200

    feat: Make some changes in the initial configuration of the visual elements (e.g. font size) feat: Don't set fixed initial values for dynamic labels, since they got set in most cases automatically, when the assigned widgets get invoked

--- sdf_simulator.py
+++ sdf_simulator.py
@@ -20,23 +20,28 @@ if __name__ == "__main__":
               meas_buf_max=10)
 
     # Add vehicles
+    # ------------
     vehicle1 = Vehicle("Vehicle1", True, 300.0, 9.0)
     gui.add_vehicle(vehicle1, color="black")
     vehicle2 = Vehicle("Vehicle2", False, 200.0, 20.0)
     gui.add_vehicle(vehicle2, color="red")
 
     # Add sensors
+    # -----------
+    # > Add sensor
     sensor = Plane("P1", False, np.asarray([3000, 8000]), cov_mat=np.asarray([[100000, 80000],
                                                                             [80000, 100000]]), meas_interval=3.0)
 
-    gui.add_sensor(sensor, fill="orange", outline="white", radius=3500, n_sides=3, font_size_scale=1.2)
+    gui.add_sensor(sensor, fill="orange", outline="white", radius=3500, n_sides=3, font_size_scale=0.8)
 
+    # > Add sensor
     sensor = Plane("P2", False, np.asarray([-9000, -5000]), cov_mat=np.asarray([[100000, 30000],
                                                                               [30000, 100000]]), meas_interval=5.0)
 
     gui.add_sensor(sensor, fill="lightblue", outline="black", radius=2500, n_sides=4, font_size_scale=1.0)
     # gui.add_sensor_group(HomogeneousTriggeredSensorGroup("Group 2", radar, 5., cov_mat=None), fill="pink")
 
+    # > Add sensors
     sigma_c = 5.e2  # m
     radar_positions = [{"r_x": 0, "r_y": 0},
                        {"r_x": 10000, "r_y": -3000},
@@ -50,13 +55,14 @@ if __name__ == "__main__":
     for i in range(len(radar_positions)):
         sensor = Plane("P3 KF #{:03d}".format(i), False, np.asarray([radar_positions[i]["r_x"], radar_positions[i]["r_y"]]), meas_interval=None, cov_mat=None)
         gui.add_sensor(sensor, fill="violet", outline="black", radius=1000, n_sides=5, rot_offset=math.pi / 5,
-                       font_size_scale=0.1)
+                       font_size_scale=0.5)
         sensors.append(sensor)
     # end for
     gui.add_sensor_group(HomogeneousTriggeredSensorGroup("Multi Group", sensors, meas_interval=5., cov_mat=np.identity(2) * sigma_c ** 2), fill="pink")
 
+    # > Add sensor
     sensor = Radar("R1", True, np.asarray([4000, 7000]), cov_mat=np.asarray([[.001, 0],
-                                                                           [0, .01]]), meas_interval=12.0)
+                                                                            [0, .01]]), meas_interval=12.0)
 
     gui.add_sensor(sensor, fill="green", outline="white", radius=3500, n_sides=3, rot_offset=math.pi, font_size_scale=.7)
 
